60ced302124aec9c1d72cf80ddf1b3e96b3bfd6b
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-12-14 21:57:19 +0000

    chore: add tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -7,6 +8,7 @@ export default defineConfig({
     host: '127.0.0.1'
   },
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
